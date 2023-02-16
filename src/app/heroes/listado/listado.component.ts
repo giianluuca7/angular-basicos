e0fb2c0988bf32284940f168f7a6a403f2593a2a
Gian Luca <gorgerinogianluca@gmail.com>
2023-02-16 16:55:35 -0300

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  
+})
+export class ListadoComponent  {
+
+  heroes: string[] = ['Spiderman','Iroman', 'Hulk', 'Thor','C. America']
+  heroeBorrado: string = ''
+
+  borrarHeroe(){
+    this.heroeBorrado = this.heroes.shift() || '';
+     
+  }
+
+
+
+}
